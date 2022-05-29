c6e502e4535e792c1789bc073632a4a5e4d16282
4ndr33w <rx.081@hotmail.com>
2022-05-29 14:48:15 +0500

    Добавьте файлы проекта.

---
+++ SportEventResultsV2/DbContent/DbConnectionClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace SportEventResultsV2.DbContent
+{
+    public class DbConnectionClass
+    {
+        string CompanyTable = Properties.Resources.CompanyTable;
+        public bool SqlConnectionState_Check(MySqlConnection connection)
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static MySqlConnectionStringBuilder ConnectionStringBuilder = new MySqlConnectionStringBuilder
+        {
+            Server = "andr33w.beget.tech", //Properties.Resources.DBHost,
+            Database = "andr33w_run_db", // Properties.Resources.DBName,
+            UserID = "andr33w_run_db", // Properties.Resources.DBName,
+            Password = "BPN1VbSi", // Properties.Resources.DBPass,
+        };
+        public MySqlConnection Sql_Connection = new MySqlConnection(ConnectionStringBuilder.ConnectionString);
+
+        public DataTable CompanyListTable(string sqlCommandString)
+        {
+            DataTable companyListDT = new DataTable();
+            DataSet _dataSet = new DataSet();
+            try
+            {
+                Sql_Connection.Open();
+                while (SqlConnectionState_Check(Sql_Connection) == false)
+                {
+                    SqlConnectionState_Check(Sql_Connection);
+                    Thread.Sleep(200);
+                }
+                string companySelectSqlString = $"SELECT CompanyName FROM CompanyList";
+
+
+                MySqlCommand companySelectCommand = new MySqlCommand(companySelectSqlString, Sql_Connection);
+                MySqlDataAdapter _adapter = new MySqlDataAdapter(companySelectCommand);
+                //companySelectCommand.ExecuteNonQuery();
+                _adapter.Fill(_dataSet, "CompanyList");
+                companyListDT = _dataSet.Tables["CompanyList"];
+                Sql_Connection.Close();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("проблемы с подключением");
+                return null;
+            }
+
+            return companyListDT;
+        }
+    }
+}
---
+++ SportEventResultsV2/DbContent/SqlStrings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportEventResultsV2.DbContent
+{
+    public class SqlStrings
+    {
+        public static string selectedGender = "";
+        public static string selectedCompany = "";
+        private string _allResults = "SELECT name, city, company, result, gender, updatedAt FROM UserSchemas";
+        private string _resultsWithGenderFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE gender = '{selectedGender}'";
+        private string _resultsWithCompanyFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE company = '{selectedCompany}'";
+        private string _resultsWithCompanyAndGenderFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE gender = '{selectedGender}' AND company = '{selectedCompany}'";
+
+        public string SqlString(string gender, string company)
+        {
+            if ((gender == string.Empty || gender == "все" || gender == null) && (company == string.Empty || company == "Все предприятия" || company == null))
+            {
+                return _allResults;
+            }
+            else if (gender != string.Empty && gender != "все" && gender != null && (company == string.Empty || company == "Все предприятия" || company == null))
+            {
+                selectedGender = gender;
+                return _resultsWithGenderFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE gender = '{selectedGender}'";
+            }
+            else if ((gender == string.Empty || gender == "все") && (company != string.Empty && company != "Все предприятия"))
+            {
+                selectedCompany = company;
+                return _resultsWithCompanyFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE company = '{selectedCompany}'";
+            }
+            else
+            {
+                selectedGender = gender;
+                selectedCompany = company;
+                return _resultsWithCompanyAndGenderFilter = $"SELECT name, city, company, result, gender, updatedAt FROM UserSchemas WHERE gender = '{selectedGender}' AND company = '{selectedCompany}'";
+            }
+        }
+    }
+}
